3578e64815e82ae4c864f0de187c6d45a63b3698
Jacob King <jakeking721@gmail.com>
2025-08-18 18:17:42 -0700

    Add Supabase config in dev mode

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,6 +1,24 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient, type SupabaseClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+declare global {
+  interface Window {
+    supabase?: SupabaseClient;
+    __ENV?: {
+      SUPABASE_URL: string;
+      SUPABASE_ANON_KEY: string;
+    };
+  }
+}
+
+if (import.meta.env.DEV && typeof window !== "undefined") {
+  window.supabase = supabase;
+  window.__ENV = {
+    SUPABASE_URL: supabaseUrl,
+    SUPABASE_ANON_KEY: supabaseAnonKey,
+  };
+}
